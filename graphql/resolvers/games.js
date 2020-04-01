8af6b683a96a982953226b940358deb18aeaae85
Dawid Kawiak <d.kawiak@selleo.com>
2020-04-01 16:05:15 +0200

    Create game, delete game

--- graphql/resolvers/games.js
+++ graphql/resolvers/games.js
@@ -1,13 +1,58 @@
 const Game = require('../../models/Game')
+const checkAuth = require('../../utils/check-auth')
 
 module.exports = {
   Query: {
     async getGames() {
       try {
-        const games = await Game.find()
+        const games = await Game.find().sort({ createdAt: -1 })
         return games
       } catch (error) {
         throw new Error(error)
+      }
+    },
+
+    async getGame(_, { postId }) {
+      try {
+        const game = await Game.findById(postId)
+        if (game) {
+          return game
+        } else {
+          throw new Error('Game not found')
+        }
+      } catch (error) {
+        throw new Error(error)
+      }
+    }
+  },
+
+  Mutation: {
+    async createGame(_, { place }, context) {
+      const user = checkAuth(context)
+      const newGame = new Game({
+        place,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString()
+      })
+
+      const game = await newGame.save()
+      return game
+    },
+
+    async deleteGame(_, { gameId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const game = await Game.findById(gameId);
+        if (user.username === game.username) {
+          await game.delete();
+          return 'Game deleted successfully';
+        } else {
+          throw new AuthenticationError('Action not allowed');
+        }
+      } catch (err) {
+        throw new Error(err);
       }
     }
   }
--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,11 +1,12 @@
-const gamesResolvers = require('./games');
-const usersResolvers = require('./users');
+const gamesResolvers = require('./games')
+const usersResolvers = require('./users')
 
 module.exports = {
   Query: {
-    ...gamesResolvers.Query
+    ...gamesResolvers.Query,
   },
   Mutation: {
-    ...usersResolvers.Mutation
+    ...usersResolvers.Mutation,
+    ...gamesResolvers.Mutation,
   }
 };
